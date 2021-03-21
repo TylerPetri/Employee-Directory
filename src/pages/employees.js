1d511068892089e2aad144d6640302d46bb53839
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-21 18:45:39 -0400

    api fetches 30 employees & sets employees state

---
+++ src/pages/employees.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+
+function getEmployees () {
+        return axios.get("https://randomuser.me/api/?results=30")
+}
+
+
+function Employees() {
+    const [employees, setEmployees] = useState([])
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        getEmployees()
+        .then((res) => {
+            if (res.data.status === "error") {
+                throw new Error(res.data.message)
+            } else {
+            setEmployees(res.data.results)
+            console.log(res.data.results)
+        }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+    return (
+        <>
+        <button type="submit" onClick={handleFormSubmit}>Search</button>
+        {/* <table>
+            <tbody>
+            <tr>
+                <th>employees</th>
+                <th>{employees[0].name}</th>
+                <th>{employees[0].location}</th>
+            </tr>
+            </tbody>
+        </table> */}
+        </>
+    )
+}
+
+export default Employees;
